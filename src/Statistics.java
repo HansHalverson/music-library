739ef904c4790e4463381e998b864ce080053ee0
HansHalverson <halverson.beattie@gmail.com>
2015-03-15 21:21:42 -0400

    Added genre statistics

--- src/Statistics.java
+++ src/Statistics.java
@@ -34,20 +34,22 @@ public class Statistics {
 			double ratio1 = 0;
 			double ratio2 = 0;
 			if (song1.getPlays() != 0) {
-				ratio1 = song1.getSkips() / song1.getPlays();
+				ratio1 = (double)song1.getSkips() / song1.getPlays();
 			}
 			if (song2.getPlays() != 0) {
-				ratio2 = song2.getSkips() / song2.getPlays();
+				ratio2 = (double)song2.getSkips() / song2.getPlays();
 			}
 			if (ratio1 > ratio2) {
 				return -1;
 			} else if (ratio1 < ratio2) {
 				return 1;
 			} else {
-				if (song1.getPlays() > song2.getPlays()) {
+				if (song1.getSkips() > song2.getSkips()) {
 					return -1;
-				} else if (song1.getPlays() < song2.getPlays()) {
+				} else if (song1.getSkips() < song2.getSkips()) {
 					return 1;
+				} else if (ratio1 == 0) {
+					return Integer.compare(song2.getPlays(), song1.getPlays());
 				}
 				return 0;
 			}
@@ -68,7 +70,7 @@ public class Statistics {
 			int i = 1;
 			for (MusicFile song : songList) {
 				StringBuilder songInfo = new StringBuilder(String.valueOf(i)).append(". ");
-				songInfo.append(song.getName()).append(" - ").append(song.getAlbumArtist()).append("     ");
+				songInfo.append(song.getName()).append(" - ").append(song.getArtist()).append("     ");
 				songInfo.append(song.getSkips()).append("/").append(song.getPlays());
 				outStream.write(songInfo.toString());
 				outStream.newLine();
@@ -119,7 +121,7 @@ public class Statistics {
 				Duration minutes = totalTime.minusHours(totalTime.toHours());
 				Duration seconds = minutes.minusMinutes(minutes.toMinutes());
 				StringBuilder songInfo = new StringBuilder(String.valueOf(i)).append(". ");
-				songInfo.append(song.getName()).append(" - ").append(song.getAlbumArtist());
+				songInfo.append(song.getName()).append(" - ").append(song.getArtist());
 				songInfo.append(" - ").append(totalTime.toHours());
 				songInfo.append(" Hours ").append(minutes.toMinutes()).append(" Minutes ");
 				songInfo.append(seconds.toMillis() / 1000).append(" Seconds");
@@ -140,7 +142,7 @@ public class Statistics {
 	private static class ContainerCompare implements Comparator<ContainerCompare> {
 		
 		String name;
-		int time;
+		long time;
 		
 		public ContainerCompare(String name, int time) {
 			this.name = name;
@@ -168,11 +170,11 @@ public class Statistics {
 	public static void totalTimePerArtist(HashMap<Integer, MusicFile> collection) {
 		HashMap<String, ContainerCompare> artists = new HashMap<String, ContainerCompare>();
 		for (MusicFile song : collection.values()) {
-			if (artists.containsKey(song.getAlbumArtist())) {
-				artists.get(song.getAlbumArtist()).time += song.getTime() * song.getPlays();
+			if (artists.containsKey(song.getArtist())) {
+				artists.get(song.getArtist()).time += song.getTime() * song.getPlays();
 			} else {
-				ContainerCompare artist = new ContainerCompare(song.getAlbumArtist(), song.getTime() * song.getPlays());
-				artists.put(song.getAlbumArtist(), artist);
+				ContainerCompare artist = new ContainerCompare(song.getArtist(), song.getTime() * song.getPlays());
+				artists.put(song.getArtist(), artist);
 			}
 		}
 		ContainerCompare[] artistArray = new ContainerCompare[artists.size()];
@@ -242,6 +244,47 @@ public class Statistics {
 	}
 	
 	/**
+	 * Writes a text file containing all genres ordered by the total time spent
+	 * listening to them
+	 * 
+	 * @param collection - the collection of songs to be sorted
+	 */
+	public static void totalTimePerGenre(HashMap<Integer, MusicFile> collection) {
+		HashMap<String, ContainerCompare> genres = new HashMap<String, ContainerCompare>();
+		for (MusicFile song : collection.values()) {
+			if (genres.containsKey(song.getGenre())) {
+				genres.get(song.getGenre()).time += song.getTime() * song.getPlays();
+			} else {
+				ContainerCompare genre = new ContainerCompare(song.getGenre(), song.getTime() * song.getPlays());
+				genres.put(song.getGenre(), genre);
+			}
+		}
+		ContainerCompare[] genreArray = new ContainerCompare[genres.size()];
+		genres.values().toArray(genreArray);
+		Arrays.sort(genreArray, new ContainerCompare("",0));
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter("Total Time Per Genre.txt"));
+			int i = 1;
+			for (ContainerCompare genre : genreArray) {
+				Duration totalTime = Duration.ofMillis(genre.time);
+				Duration minutes = totalTime.minusHours(totalTime.toHours());
+				Duration seconds = minutes.minusMinutes(minutes.toMinutes());
+				StringBuilder artistInfo = new StringBuilder(String.valueOf(i));
+				artistInfo.append(". ").append(genre.name).append(" - ").append(totalTime.toHours());
+				artistInfo.append(" Hours ").append(minutes.toMinutes()).append(" Minutes ");
+				artistInfo.append(seconds.toMillis() / 1000).append(" Seconds");
+				writer.write(artistInfo.toString());
+				writer.newLine();
+				i++;
+			}
+			writer.close();
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+			System.out.println(e.getStackTrace());
+		}
+	}
+	
+	/**
 	 * Finds the total time spent listening to music in the provided collection
 	 * 
 	 * @param collection - the collection of songs to be analyzed
@@ -271,6 +314,7 @@ public class Statistics {
 		totalTimePerSong(library);
 		totalTimePerArtist(library);
 		totalTimePerAlbum(library);
+		totalTimePerGenre(library);
 	}
 
 }
